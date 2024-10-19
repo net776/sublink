335f69c3696a3328a9bc470d92ed7417fc8e2446
mfbpn <141901587+mfbpn@users.noreply.github.com>
2024-10-19 14:31:28 +0800

    Update bzy.py

--- utils/bzy.py
+++ utils/bzy.py
@@ -99,5 +99,5 @@ if __name__ == '__main__':
     with open("./links/ss", "wb") as f:
             f.write(abcd3)
     message = '#SS ' + '#订阅' + '\n' + datetime.now().strftime(
-            "%Y年%m月%d日%H:%M:%S") + '\n' + 'bzy订阅每天自动更新：' + '\n' + 'https://raw.githubusercontent.com/mfbpn/TrojanLinks/master/links/ss'
+            "%Y年%m月%d日%H:%M:%S") + '\n' + 'bzy订阅每天自动更新：' + '\n' + 'https://raw.githubusercontent.com/mfbpn/sublink/master/links/ss'
     send_message(os.environ['chat_id'], message, os.environ['bot_token'])
