06e3e887d169a0b01a1db42e09362d9395cfa16f
Fan Ruina <20201b1029@stu.peihua.edu.cn>
2024-10-29 20:43:40 +0800

    Update skr.py

--- utils/skr.py
+++ utils/skr.py
@@ -14,11 +14,14 @@ import binascii
 from datetime import datetime
 
 if __name__ == '__main__':
-    a = os.environ['skr_a']
-    c = os.environ['skr_c']
-    d = os.environ['skr_d']
-    e = os.environ['skr_e']
-    b = {
+    # 获取环境变量
+    地址 = os.environ['skr_a']
+    请求数据 = os.environ['skr_c']
+    密钥 = os.environ['skr_d']
+    初始化向量 = os.environ['skr_e']
+
+    # 请求头
+    请求头 = {
         'accept': '/',
         'accept-language': 'zh-Hans-CN;q=1, en-CN;q=0.9',
         'appversion': '1.3.1',
@@ -27,26 +30,27 @@ if __name__ == '__main__':
         'Cookie': 'PHPSESSID=fnffo1ivhvt0ouo6ebqn86a0d4'
     }
 
-def f(g, d, e):
-    h = pyaes.AESModeOfOperationCBC(d, iv=e)
-    i = b''.join(h.decrypt(g[j:j+16]) for j in range(0, len(g), 16))
-    return i[:-i[-1]]
+    # AES解密函数
+    def 解密(加密数据, 密钥, 初始化向量):
+        aes = pyaes.AESModeOfOperationCBC(密钥, iv=初始化向量)
+        解密数据 = b''.join(aes.decrypt(加密数据[i:i+16]) for i in range(0, len(加密数据), 16))
+        return 解密数据[:-解密数据[-1]]
 
-j = requests.post(a, headers=b, data=c)
-skr = ''
-if j.status_code == 200:
-    k = j.text.strip()
-    l = binascii.unhexlify(k)
-    m = f(l, d.encode(), e.encode())
-    n = json.loads(m)
-    for o in n['data']:
-        p = f"aes-256-cfb:{o['password']}@{o['ip']}:{o['port']}"
-        q = base64.b64encode(p.encode('utf-8')).decode('utf-8')
-        r = f"ss://{q}#{o['title']}"
-        skr += r + '\n'
-    print(skr)
-    with open("./links/skr", "w") as f:
-        f.write(base64.b64encode(skr.encode()).decode())
-    message = '#ss ' + '#订阅' + '\n' + datetime.now().strftime("%Y年%m月%d日%H:%M:%S") + '\n' + 'skr订阅每天自动更新：' + '\n' + 'https://raw.githubusercontent.com/mfbpn/TrojanLinks/master/links/skr'
-    send_message(os.environ['chat_id'], message, os.environ['bot_token'])
-  
+    # 发送POST请求
+    响应 = requests.post(地址, headers=请求头, data=请求数据)
+    订阅链接 = ''
+    if 响应.status_code == 200:
+        响应文本 = 响应.text.strip()
+        解密后的数据 = binascii.unhexlify(响应文本)
+        解密后的文本 = 解密(解密后的数据, 密钥.encode(), 初始化向量.encode())
+        数据 = json.loads(解密后的文本)
+        for 项目 in 数据['data']:
+            ss链接 = f"aes-256-cfb:{项目['password']}@{项目['ip']}:{项目['port']}"
+            base64编码 = base64.b64encode(ss链接.encode('utf-8')).decode('utf-8')
+            完整链接 = f"ss://{base64编码}#{项目['title']}"
+            订阅链接 += 完整链接 + '\n'
+        print(订阅链接)
+        with open("./links/skr", "w") as 文件:
+            文件.write(base64.b64encode(订阅链接.encode()).decode())
+        消息 = '#ss ' + '#订阅' + '\n' + datetime.now().strftime("%Y年%m月%d日%H:%M:%S") + '\n' + 'skr订阅每天自动更新：' + '\n' + 'https://raw.githubusercontent.com/mfbpn/TrojanLinks/master/links/skr'
+        send_message(os.environ['chat_id'], 消息, os.environ['bot_token'])
